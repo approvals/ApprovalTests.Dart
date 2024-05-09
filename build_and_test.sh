134b35c13dba6e3a60447b26e307ae587efb7e57
Yelaman Yelmuratov <e99xxx@gmail.com>
2024-05-10 00:00:43 +0500

    . e added activate command to shell script

--- build_and_test.sh
+++ build_and_test.sh
@@ -3,4 +3,5 @@
 dart pub get
 dart format .
 dart analyze
+dart pub global activate coverage
 dart run test --coverage=./coverage && dart pub global run coverage:format_coverage --packages=.dart_tool/package_config.json --report-on=lib --lcov -o ./coverage/lcov.info -i ./coverage
